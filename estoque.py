8c449e4a860fadafd1d2503691fd2b9b86fb9e81
Alessandro Teixeira da Silva <100613931+alessandro-tsilva@users.noreply.github.com>
2023-04-01 15:52:38 -0300

    Add files via upload

---
+++ estoque.py
@@ -0,0 +1,17 @@
+print ("Controle de estoque")
+print (". . . . .")
+def estoque():
+    cont = "S"
+    est = 0
+    while cont == "S":
+        prod = input("Digite o produto: ")
+        qtde = int(input("Digite a quantidade: "))
+        cont = input("Continuar? S/N: ")
+        est = est + qtde
+    return est
+
+estoq = str(estoque())
+print (". . . ")
+print (f"O total de produtos em estoque é: " + estoq)
+print (" Fim do programa")
+
